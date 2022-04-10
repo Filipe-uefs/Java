81f53acb66c93b8666c8143c8e3280b43e4a7079
Filipe Pereira da Silva <lipe_0109@hotmail.com>
2022-04-10 11:08:02 -0300

    create class contaCorrente

---
+++ Coleçõs, Wrappes e Lambadas expressions/src/ContaCorrente.java
@@ -0,0 +1,28 @@
+public class ContaCorrente {
+
+    int numero;
+    float saldo;
+
+    public ContaCorrente () {}
+
+    public ContaCorrente(int numero, float saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+}
