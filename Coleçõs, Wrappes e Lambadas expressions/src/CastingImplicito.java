b04f726f5e83ebda358d6ee3b261864a782cebad
Filipe Pereira da Silva <lipe_0109@hotmail.com>
2022-04-10 11:12:22 -0300

    casting implicito

---
+++ Coleçõs, Wrappes e Lambadas expressions/src/CastingImplicito.java
@@ -0,0 +1,9 @@
+public class CastingImplicito {
+
+    public static void main(String[] args) {
+        int numero = 14;
+        double numeroDouble = numero;
+
+        System.out.println(numeroDouble);
+    }
+}
