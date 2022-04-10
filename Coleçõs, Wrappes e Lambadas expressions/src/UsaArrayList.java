6233bf84fd172a42e4b3ef6e18c94764a3a226c1
Filipe Pereira da Silva <lipe_0109@hotmail.com>
2022-04-10 20:18:57 -0300

    array list with generic

---
+++ Coleçõs, Wrappes e Lambadas expressions/src/UsaArrayList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class UsaArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<ContaCorrente> lista = new ArrayList<>();
+        ContaCorrente conta1 = new ContaCorrente(103333, 1000F);
+        ContaCorrente conta2 = new ContaCorrente(104444, 5500F);
+        ContaCorrente conta3 = new ContaCorrente(1044422, 54500F);
+        lista.add(conta1);
+        lista.add(conta2);
+        lista.add(conta3);
+        int tamanho = lista.size();
+        System.out.println("Tamanho da lista " + tamanho);
+        lista.remove(0);
+        tamanho = lista.size();
+        System.out.println("Novo tamanho da lista " + tamanho);
+
+        for(ContaCorrente conta: lista) {
+            System.out.println("Numero da conta: " + conta.getNumero() + " Saldo: " + conta.getSaldo());
+        }
+    }
+}
