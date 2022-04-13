ef668415e42adfaf331232ae9f9a84935377a5f8
Filipe Pereira da Silva <lipe_0109@hotmail.com>
2022-04-13 11:13:33 -0300

    create Model Produto

---
+++ JPA_HIBERNATE/src/main/java/Model/Produto.java
@@ -0,0 +1,63 @@
+package Model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+
+    private String descricao;
+
+    @Column(name = "codigo_de_barra")
+    private int codigoDeBarra;
+
+    private BigDecimal preco;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCodigoDeBarra() {
+        return codigoDeBarra;
+    }
+
+    public void setCodigoDeBarra(int codigoDeBarra) {
+        this.codigoDeBarra = codigoDeBarra;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+}
